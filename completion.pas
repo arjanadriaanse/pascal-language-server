85e4d45430a24ce45c5e220505ad7731af7c8f3e
Arjan Adriaanse <arjan@adriaan.se>
2020-04-22 23:19:13 +0200

    add license

--- completion.pas
+++ completion.pas
@@ -1,3 +1,22 @@
+// Pascal Language Server
+// Copyright 2020 Arjan Adriaanse
+
+// This file is part of Pascal Language Server.
+
+// Pascal Language Server is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+
+// Pascal Language Server is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Pascal Language Server.  If not, see
+// <https://www.gnu.org/licenses/>.
+
 unit completion;
 
 {$mode objfpc}{$H+}
